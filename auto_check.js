9bedf24af7358d88a14ec26ad18828696280498d
IPP <1182896819@qq.com>
2020-01-25 22:06:57 +0800

    V0.1
    F12控制台粘贴回车即可~
    参考https://blog.csdn.net/qq_34178598/article/details/78801427，欢迎交流

---
+++ auto_check.js
@@ -0,0 +1,36 @@
+    alert("Powered By IPP.仅供学习使用~");
+    var iframe = window.top.document.getElementById('iframeautoheight');
+    var ifdocument = iframe.contentWindow.document;	
+    var subLength = ifdocument.getElementById("pjkc").length - 1;//评价课程选项个数
+    console.log('共%d个选项',subLength);
+    ifdocument.getElementById("TextBox1").value = - 1;
+    function autoexecution()
+        {
+            var iframe = window.top.document.getElementById('iframeautoheight');
+            var ifdocument = iframe.contentWindow.document;
+            var reg1 = /^DataGrid1__ctl\d+_JS\d+$/i;
+            var selections = ifdocument.getElementsByTagName("select");
+            var arr = [true, false];
+            for(var i = 0 ; i< selections.length; i++)
+            {
+			 	if(reg1.test(selections[i].getAttribute("id")))
+                {
+			 		if(arr[Math.floor(Math.random()*arr.length)])
+                        selections[i].value = "A";
+                    else
+                        selections[i].value = "B";
+			 	}
+			}
+            ifdocument.getElementById("Button1").click();
+		}
+			
+			autoexecution();
+    iframe.onload = function()
+    {
+        if(subLength -- > 0)
+        {
+			console.log("auto jumping.....");
+            console.log("少女祈祷中.....");
+            autoexecution();
+		}
+	}
